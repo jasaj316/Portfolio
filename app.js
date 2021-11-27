c59266cb8a5f3c345840e8c06e48f4a2ea023cbf
Jsmith221 <jsmth216@gmail.com>
2021-11-27 16:32:41 -0500

    Changed indexc to about page

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var aboutRouter = require('./routes/about');
 var projectsRouter = require('./routes/projects');
 var contactRouter = require('./routes/contact');
 // External links router
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/about', aboutRouter);
 app.use('/projects', projectsRouter);
 app.use('/contact', contactRouter);
 // External links router
