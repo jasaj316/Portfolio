d50112f8379b0a0b141168f19a32bd502f9cad6f
Jsmith221 <jsmth216@gmail.com>
2021-12-01 21:07:55 -0500

    adding modal image viewer

--- public/javascripts/projectsScripts.js
+++ public/javascripts/projectsScripts.js
@@ -73,7 +73,7 @@ window.addEventListener("click", (e) => {
     removeModal();
   }
   //if clicking img, add a modal window
-  if (e.target.nodeName == "IMG" && !e.target.hasAttribute("")) {
+  if (e.target.nodeName == "IMG" && !e.target.hasAttribute("id")) {
     addModal(e)
   }
   else if (document.querySelector(".modal-bg")) {
