ee5ed9ee271d7945d1607d7959ff78aa4e0cacad
Jsmith221 <jsmth216@gmail.com>
2021-12-01 20:23:20 -0500

    adding modal image viewer

--- public/javascripts/projectsScripts.js
+++ public/javascripts/projectsScripts.js
@@ -56,8 +56,10 @@ linkTwo.addEventListener("click", () => {
 
 // Modal Images
 let selectedImg = "";
-const modalElements = `<div class="modal-bg"></div>
+let modalElements = `<div class="modal-bg"></div>
 <img src="${selectedImg}" alt="" class="modal-img">`;
+let modal = document.createAttribute("modal");
+modal.value = modalElements;
 
 function removeModal() {
   let oldBg = document.querySelector(".modal-bg");
@@ -76,7 +78,7 @@ window.addEventListener("click", (e) => {
     // add the target src to the modal element
     selectedImg = e.target.src;
     // insert modal element
-    document.insertBefore(modalElements, itemsOne);
+    document.insertBefore(modal, itemsOne);
   }
   else {
     // if not clicking an image, remove modal
