397600ed4cac379a9ed47e20e06bd01223794d76
Jsmith221 <jsmth216@gmail.com>
2022-09-07 21:15:59 -0400

    changing modal functionality

--- public/javascripts/projectsScripts.js
+++ public/javascripts/projectsScripts.js
@@ -76,7 +76,7 @@ function removeModal() {
 function addModal(e) {
   //select main and insert the modal "beforeend"
   let main = document.querySelector("main");
-  main.insertAdjacentHTML("beforeend", `<div class="modal-bg"></div><img src="${e.target.src.slice(34)}" class="modal-img"><p class="modal-x">✖</p>`);
+  main.insertAdjacentHTML("beforeend", `<div class="modal-bg"></div><img src="${e.target.src.slice(34)}" class="modal-img">`);
   //modalActive is true
   modalActive = true;
 }
