4aef3ddabd07f9b7508f7e70e099f11cd0de817e
Jsmith221 <jsmth216@gmail.com>
2024-05-09 20:53:16 -0400

    improved arrow functionality and responsive scaling

--- public/javascripts/scripts/contactScripts.js
+++ public/javascripts/scripts/contactScripts.js
@@ -1,8 +1,8 @@
 export function contactScripts() {
-  let formButton = document.querySelector(".form-button");
-
   // populate mailto link with form data
-  formButton.addEventListener("click", () => {
+  document.querySelector("#form-button").addEventListener("click", () => {
     formButton.href = `mailto:jasaj316@gmail.com?subject=${subject.value}&body=${body.value}`;
   })
+  // populate email text
+  document.querySelector("#form-email span").innerHTML = "jasaj316@gmail.com";
 }
--- public/javascripts/scripts/projectsScripts.js
+++ public/javascripts/scripts/projectsScripts.js
@@ -1,42 +1,56 @@
 export function projectsScripts() {
+  // modal elements
   let modal = document.querySelector("#modal");
   let modalImg = document.querySelector("#modal>div img");
+  let modalTitle = document.querySelector("#modal>div h2");
   let modalLb = document.querySelector("#modal>div p:first-of-type");
   let modalRb = document.querySelector("#modal>div p:last-of-type");
 
-  // get an array of all card images
-  let cardImgs = document.querySelectorAll(".card img");
+  // an array of all card titles and their images
+  let cards = [];
+  document.querySelectorAll(".card").forEach(card => {
+    cards.push({ title: card.querySelector("h3"), img: card.querySelector("img") })
+  });
 
-  function loadImg(e) {
+  function loadImg(e, dir = "") {
+    // if clicking card img, show that img
     if (e.target.nodeName === "IMG" && e.target.parentNode.className === "card") {
       modalImg.src = e.target.src;
     }
-    else if (e.target == modalLb) {
-      for (let i = 0; i < cardImgs.length; i++) {
-        if (modalImg.src === cardImgs[i].src)
+    // else if using arrows, show previous or next img
+    else if (dir === "L") {
+      for (let i = 0; i < cards.length; i++) {
+        if (modalImg.src === cards[i].img.src)
           if (i > 0) {
-            modalImg.src = cardImgs[i - 1].src;
+            modalImg.src = cards[i - 1].img.src;
             break;
           }
           else {
-            modalImg.src = cardImgs[cardImgs.length - 1].src;
+            modalImg.src = cards[cards.length - 1].img.src;
             break;
           }
       }
     }
-    else if (e.target == modalRb) {
-      for (let i = 0; i < cardImgs.length; i++) {
-        if (modalImg.src === cardImgs[i].src)
-          if (i < cardImgs.length - 1) {
-            modalImg.src = cardImgs[i + 1].src;
+    else if (dir === "R") {
+      for (let i = 0; i < cards.length; i++) {
+        if (modalImg.src === cards[i].img.src)
+          if (i < cards.length - 1) {
+            modalImg.src = cards[i + 1].img.src;
             break;
           }
           else {
-            modalImg.src = cardImgs[0].src;
+            modalImg.src = cards[0].img.src;
             break;
           }
       }
     }
+    // assign title according to current img
+    cards.forEach(card => {
+      if (modalImg.src === card.img.src) {
+        modalTitle.innerHTML = card.title.innerHTML;
+      }
+    })
+
     // stop modal listener from receiving event
     e.stopPropagation();
   }
@@ -51,13 +65,22 @@ export function projectsScripts() {
     modal.showModal();
   });
 
-  //when clicking left/right, set the modal image's src to tehe privious or next card img's src
+  //when going left/right, set the modal image's src to the privious or next card img's src
   modalLb.addEventListener("click", (e) => {
-    loadImg(e);
+    loadImg(e, "L");
   });
   modalRb.addEventListener("click", (e) => {
-    loadImg(e);
+    loadImg(e, "R");
   });
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft")
+      loadImg(e, "L");
+
+    else if (e.key === "ArrowRight")
+      loadImg(e, "R");
+  });
+
+
   // when clicking on a card img, set the modal image's src to the card img's src
   document.addEventListener("click", (e) => {
     loadImg(e);
